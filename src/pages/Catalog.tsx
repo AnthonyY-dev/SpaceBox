82e7d0d295861e3a0a3459498d9b75bfa2d74a1e
Anthony Y <toha.yurynets@gmail.com>
2025-01-29 15:36:19 -0500

    Add accordian, boxes and spaces to catalog

--- src/pages/Catalog.tsx
+++ src/pages/Catalog.tsx
@@ -12,24 +12,51 @@ import React, { useEffect, useState } from "react";
 import supabase from "@/hooks/supabase";
 import { Link } from "react-router-dom";
 import Item from "@/types/Item";
-import { LuX } from "react-icons/lu";
+import { LuApple, LuHammer, LuPackage, LuSpace, LuX } from "react-icons/lu";
+import {
+  AccordionItem,
+  AccordionItemContent,
+  AccordionItemTrigger,
+  AccordionRoot,
+} from "@/components/ui/accordion";
+import Box from "@/types/Box";
+import Space from "@/types/Space";
 
 const Catalog = () => {
   var accessCode = useAuthenticated();
 
-  const [data, setData] = useState<Item[] | null>(null);
+  const [dbItemData, setDbItemData] = useState<Item[] | null>(null);
+  const [dbBoxesData, setDbBoxesData] = useState<Box[] | null>(null);
+  const [dbSpacesData, setDbSpacesData] = useState<Space[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<string>("");
   useEffect(() => {
     async function loadItems() {
-      const { data, error } = await supabase.from("items").select("*");
-      if (error) {
-        setError(error.message);
-        setLoading(false);
+      const { data: itemsData, error: error1 } = await supabase
+        .from("items")
+        .select("*");
+      const { data: boxesData, error: error2 } = await supabase
+        .from("boxes")
+        .select("*");
+      const { data: spacesData, error: error3 } = await supabase
+        .from("spaces")
+        .select("*");
+      if (error1) {
+        setError(error1.message);
       } else {
-        setData(data);
-        setLoading(false);
+        setDbItemData(itemsData);
       }
+      if (error2) {
+        setError(error2.message);
+      } else {
+        setDbBoxesData(boxesData);
+      }
+      if (error3) {
+        setError(error3.message);
+      } else {
+        setDbSpacesData(spacesData);
+      }
+      setLoading(false);
     }
     loadItems();
   }, []);
@@ -72,34 +99,149 @@ const Catalog = () => {
             <Text>Loading...</Text>
           </VStack>
         ) : (
-          data?.map((itemData) => (
-            <Link to={"/item/" + itemData.id + "?code=" + accessCode}>
-              <Button
-                variant="outline"
-                width="450px"
-                h="40px"
+          <AccordionRoot marginTop={"40px"} collapsible>
+            <AccordionItem value="items" key="items">
+              <AccordionItemTrigger>
+                <Button
+                  variant="outline"
+                  width="450px"
+                  h="40px"
+                  display={"flex"}
+                  flexDir={"row"}
+                  colorPalette={"green"}
+                >
+                  <LuApple color="white" />{" "}
+                  <span className="text" style={{ color: "white" }}>
+                    Items
+                  </span>
+                </Button>
+              </AccordionItemTrigger>
+              <AccordionItemContent
                 display={"flex"}
-                flexDir={"row"}
+                flexDir={"column"}
+                gap="10px"
               >
-                <Avatar
-                  name={itemData.name}
-                  shape={"rounded"}
-                  height={"80%"}
-                  width={"32px"}
-                  position={"absolute"}
-                  left="10px"
-                  src={itemData.imageUrl}
-                />
-                <Text>{itemData.name}</Text>
-              </Button>
-            </Link>
-          ))
+                {dbItemData?.map((itemData) => (
+                  <Link to={"/item/" + itemData.id + "?code=" + accessCode}>
+                    <Button
+                      variant="outline"
+                      width="450px"
+                      h="40px"
+                      display={"flex"}
+                      flexDir={"row"}
+                    >
+                      <Avatar
+                        name={itemData.name}
+                        shape={"rounded"}
+                        height={"80%"}
+                        width={"32px"}
+                        position={"absolute"}
+                        left="10px"
+                        src={itemData.imageUrl}
+                      />
+                      <Text>{itemData.name}</Text>
+                    </Button>
+                  </Link>
+                ))}
+              </AccordionItemContent>
+            </AccordionItem>
+            <AccordionItem value="boxes" key="boxes">
+              <AccordionItemTrigger>
+                <Button
+                  variant="outline"
+                  width="450px"
+                  h="40px"
+                  display={"flex"}
+                  flexDir={"row"}
+                  colorPalette={"yellow"}
+                >
+                  <LuPackage color="white" />{" "}
+                  <span className="text" style={{ color: "white" }}>
+                    Boxes
+                  </span>
+                </Button>
+              </AccordionItemTrigger>
+              <AccordionItemContent
+                display={"flex"}
+                flexDir={"column"}
+                gap="10px"
+              >
+                {dbBoxesData?.map((boxData) => (
+                  <Link to={"/box/" + boxData.id + "?code=" + accessCode}>
+                    <Button
+                      variant="outline"
+                      width="450px"
+                      h="40px"
+                      display={"flex"}
+                      flexDir={"row"}
+                    >
+                      <Avatar
+                        name={boxData.boxName}
+                        shape={"rounded"}
+                        height={"80%"}
+                        width={"32px"}
+                        position={"absolute"}
+                        left="10px"
+                        src={boxData.boxImageUrl}
+                      />
+                      <Text>{boxData.boxName}</Text>
+                    </Button>
+                  </Link>
+                ))}
+              </AccordionItemContent>
+            </AccordionItem>
+            <AccordionItem value="spaces" key="spaces">
+              <AccordionItemTrigger>
+                <Button
+                  variant="outline"
+                  width="450px"
+                  h="40px"
+                  display={"flex"}
+                  flexDir={"row"}
+                  colorPalette={"blue"}
+                >
+                  <LuSpace color="white" />{" "}
+                  <span className="text" style={{ color: "white" }}>
+                    Spaces
+                  </span>
+                </Button>
+              </AccordionItemTrigger>
+              <AccordionItemContent
+                display={"flex"}
+                flexDir={"column"}
+                gap="10px"
+              >
+                {dbSpacesData?.map((spaceData) => (
+                  <Link to={"/space/" + spaceData.id + "?code=" + accessCode}>
+                    <Button
+                      variant="outline"
+                      width="450px"
+                      h="40px"
+                      display={"flex"}
+                      flexDir={"row"}
+                    >
+                      <Avatar
+                        name={spaceData.spaceName}
+                        shape={"rounded"}
+                        height={"80%"}
+                        width={"32px"}
+                        position={"absolute"}
+                        left="10px"
+                        src={spaceData.spaceImageUrl}
+                      />
+                      <Text>{spaceData.spaceName}</Text>
+                    </Button>
+                  </Link>
+                ))}
+              </AccordionItemContent>
+            </AccordionItem>
+          </AccordionRoot>
         )}
       </Flex>
       <Link to={"/dashboard?code=" + accessCode}>
         <IconButton
           position={"fixed"}
-          right="10px"
+          right="20px"
           top="10px"
           variant={"subtle"}
         >
--- src/types/Space.ts
+++ src/types/Space.ts
@@ -1,6 +1,7 @@
-export default interface Box{
+export default interface Space{
     id: string,
     created_at: string,
     boxes: string[],
     spaceImageUrl: string,
+    spaceName: string
 }
